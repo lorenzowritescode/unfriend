f0d5623bf7cd57b9b7768e63ff9321bd1dd35624
Lorenzo Paoliani <lorenzo.paoliani13@imperial.ac.uk>
2015-03-21 19:02:30 +0000

    Implemented list view with loader, stil not sure why there are so many errors

--- app/src/main/java/io/corn/uni/unfriend/TwitterActivity.java
+++ app/src/main/java/io/corn/uni/unfriend/TwitterActivity.java
@@ -1,10 +1,14 @@
 package io.corn.uni.unfriend;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.view.View;
+
+import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
@@ -12,24 +16,71 @@ import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 
-public class TwitterActivity extends ActionBarActivity {
+public class TwitterActivity extends ActionBarActivity implements TwitterList.OnFragmentInteractionListener{
     private TwitterLoginButton loginButton;
+    private View twitter_list;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter);
 
-        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
-        loginButton.setCallback(new Callback<TwitterSession>() {
-            @Override
-            public void success(Result<TwitterSession> result) {
-                // Do something with result, which provides a TwitterSession for making API calls
-            }
+        twitter_list = findViewById(R.id.twitter_list);
+        twitter_list.setVisibility(View.GONE);
 
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setVisibility(View.GONE);
+        showAuthButton();
+
+    }
+
+    private void showAuthButton() {
+        if (!isAuthed()){
+            loginButton.setVisibility(View.VISIBLE);
+            loginButton.setCallback(new Callback<TwitterSession>() {
+                @Override
+                public void success(Result<TwitterSession> result) {
+                    Twitter.getSessionManager().setActiveSession(result.data);
+                    loginButton.setVisibility(View.GONE);
+                    goToListView();
+                }
+
+                @Override
+                public void failure(TwitterException exception) {
+                    System.err.println("Failed to login to Twitter!");
+                    // TODO: exit the app or something
+                }
+            });
+        } else {
+
+            goToListView();
+        }
+    }
+
+    private boolean isAuthed() {
+        final TwitterSession session = Twitter.getSessionManager().getActiveSession();
+        return session != null && session.getAuthToken() != null;
+    }
+    private void goToListView() {
+        assert isAuthed(): "The user should be authenticated";
+        showLogout();
+        ProgressDialog progress = new ProgressDialog(this);
+        progress.setTitle("Loading");
+        progress.setMessage("Wait while loading...");
+        progress.show();
+        twitter_list.setVisibility(View.VISIBLE);
+    }
+
+    private void showLogout() {
+        final View sign_out_bt = findViewById(R.id.twitter_signout);
+        sign_out_bt.setVisibility(View.VISIBLE);
+        sign_out_bt.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void failure(TwitterException exception) {
-                // Do something on failure
+            public void onClick(View v) {
+                Twitter.getSessionManager().clearActiveSession();
+                sign_out_bt.setVisibility(View.INVISIBLE);
+                twitter_list.setVisibility(View.GONE);
+                showAuthButton();
             }
         });
     }
@@ -61,5 +112,10 @@ public class TwitterActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
     }
 }
